8af87287603a6a80d0189b3d1945f9e1e8e33989
jakhongiromonboev <jakhongiromonboev@gmail.com>
2026-01-28 23:20:15 +0900

    feat: update core files for fairway

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,15 +6,14 @@ export default function Document() {
 			<Head>
 				<meta name="robots" content="index,follow" />
 				<link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
-
 				{/* SEO */}
-				<meta name="keyword" content={'nestar, nestar.uz, devex mern, mern nestjs fullstack'} />
+				<meta name="keyword" content={'fairway, golf equipment, golf events, golf community, golf shop, golf korea'} />
 				<meta
 					name={'description'}
 					content={
-						'Buy and sell properties anywhere anytime in South Korea. Best Properties at Best prices on nestar.uz | ' +
-						'Покупайте и продавайте недвижимость в любой точке Южной Кореи в любое время. Лучшая недвижимость по лучшим ценам на nestar.uz | ' +
-						'대한민국 언제 어디서나 부동산을 사고팔 수 있습니다. Nestar.uz에서 최적의 가격으로 최고의 부동산을 만나보세요'
+						'Buy golf equipment, join golf events, and connect with the golf community in South Korea. Best golf products and experiences on Fairway | ' +
+						'Покупайте снаряжение для гольфа, участвуйте в турнирах и общайтесь с гольф-сообществом в Южной Корее. Лучшие товары и мероприятия на Fairway | ' +
+						'대한민국에서 골프 장비를 구매하고, 골프 이벤트에 참여하고, 골프 커뮤니티와 연결하세요. Fairway에서 최고의 골프 제품과 경험을 만나보세요'
 					}
 				/>
 			</Head>
