4c1f1e926c11326329035a91ebfa504c2ca3ce8a
jakhongiromonboev <jakhongiromonboev@gmail.com>
2026-01-28 22:58:23 +0900

    feat: update utils file added product and event like handlers

--- libs/utils.ts
+++ libs/utils.ts
@@ -5,15 +5,28 @@ export const formatterStr = (value: number | undefined): string => {
 	return numeral(value).format('0,0') != '0' ? numeral(value).format('0,0') : '';
 };
 
-export const likeTargetPropertyHandler = async (likeTargetProperty: any, id: string) => {
+export const likeTargetProductHandler = async (likeTargetProduct: any, id: string) => {
 	try {
-		await likeTargetProperty({
+		await likeTargetProduct({
 			variables: {
 				input: id,
 			},
 		});
 	} catch (err: any) {
-		console.log('ERROR, likeTargetPropertyHandler:', err.message);
+		console.log('ERROR, likeTargetProductHandler:', err.message);
+		sweetMixinErrorAlert(err.message).then();
+	}
+};
+
+export const likeTargetEventHandler = async (likeTargetEvent: any, id: string) => {
+	try {
+		await likeTargetEvent({
+			variables: {
+				input: id,
+			},
+		});
+	} catch (err: any) {
+		console.log('ERROR, likeTargetEventHandler:', err.message);
 		sweetMixinErrorAlert(err.message).then();
 	}
 };
